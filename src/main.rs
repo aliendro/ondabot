64e517d43e395267467df4eb0ea8a20dc24905e0
Michael Aliendro <mgbaliendro@gmail.com>
2024-11-21 16:38:06 -0300

    pregas counter

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,37 @@
+use std::sync::Arc;
+
 use anyhow::Context as _;
-use serenity::async_trait;
 use serenity::model::channel::Message;
 use serenity::model::gateway::Ready;
 use serenity::prelude::*;
+use serenity::{all::UserId, async_trait};
 use shuttle_runtime::SecretStore;
 use tracing::{error, info};
 
-struct Bot;
+struct Bot {
+    count: Arc<Mutex<u32>>,
+}
 
-const KAMIKAZE_ID: serenity::all::UserId = serenity::all::UserId::new(368133094831685642);
+// const GANGORRA_ID: UserId = UserId::new(183715899348353025);
+const KAMIKAZE_ID: UserId = UserId::new(368133094831685642);
 
 #[async_trait]
 impl EventHandler for Bot {
     async fn message(&self, ctx: Context, msg: Message) {
-        if msg.mentions_user_id(KAMIKAZE_ID) {
-            if let Err(why) = msg.channel_id.say(&ctx.http, "ZP mencionado!").await {
-                error!("Error sending message: {why:?}");
-            }
-        }
-
-        if msg.content == "Quantas pregas o Kamikaze tem?" {
+        if msg.content.to_lowercase().contains("prega") {
+            let mut locked_count = self.count.lock().await;
+            *locked_count += 1;
             match msg.author.id {
                 KAMIKAZE_ID => {
-                    if let Err(why) = msg
-                        .channel_id
-                        .say(&ctx.http, "Não converso com zero pregas.")
-                        .await
-                    {
+                    let response = format!("Kamikaze já perdeu {locked_count} pregas");
+
+                    if let Err(why) = msg.channel_id.say(&ctx.http, response).await {
                         error!("Error sending message: {why:?}");
                     }
+
+                    info!("Kamikaze falou prega {} vezes.", locked_count);
                 }
-                _ => {
-                    if let Err(why) = msg.channel_id.say(&ctx.http, "0 (zero), é claro!").await {
-                        error!("Error sending message: {why:?}");
-                    }
-                }
+                _ => (),
             }
         }
     }
@@ -49,6 +46,7 @@ async fn serenity(
     #[shuttle_runtime::Secrets] secrets: SecretStore,
 ) -> shuttle_serenity::ShuttleSerenity {
     // Get the discord token set in `Secrets.toml`
+    let count = Arc::new(Mutex::new(1));
     let token = secrets
         .get("DISCORD_TOKEN")
         .context("'DISCORD_TOKEN' was not found")?;
@@ -57,7 +55,9 @@ async fn serenity(
     let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
 
     let client = Client::builder(&token, intents)
-        .event_handler(Bot)
+        .event_handler(Bot {
+            count: count.clone(),
+        })
         .await
         .expect("Err creating client");
 
